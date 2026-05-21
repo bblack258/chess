6351c3687980e20042c0660567666cbc63694ccb
Ben Black <bencoltblack@gmail.com>
2026-05-20 21:03:24 -0600

    Phase 3: UserServiceTest written.

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,20 +1,94 @@
 package service;
 
+import dataaccess.*;
+import model.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
 
-    @Test
-    void register() {
+    UserDAO userMemory;
+    AuthDAO authMemory;
+    UserService service;
+    UserData newUser;
+
+    @BeforeEach
+    void setup() {
+        userMemory = new MemoryUserDAO();
+        authMemory = new MemoryAuthDAO();
+        service = new UserService(userMemory, authMemory);
+        newUser = new UserData("username","password", "email");
     }
 
     @Test
-    void login() {
+    void registerCorrect() {
+        AuthData auth;
+        try {
+            auth = service.register(newUser);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals("username", auth.username());
+        assertNotNull(auth.authToken());
     }
 
     @Test
-    void logout() {
+    void registerBad() {
+        newUser = new UserData("username", null, "email");
+        assertThrows(BadRequestException.class, () -> service.register(newUser));
+    }
+
+    @Test
+    void registerTaken() {
+        UserData secondUser = new UserData("username","otherPassword", "otherEmail");
+        try {
+            service.register(newUser);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertThrows(AlreadyTakenException.class, () -> service.register(secondUser));
+    }
+
+    @Test
+    void loginCorrect() {
+        AuthData auth;
+        try {
+            service.register(newUser);
+            auth = service.login(newUser);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals("username", auth.username());
+        assertNotNull(auth.authToken());
+    }
+
+    @Test
+    void loginUnauthorized() {
+        try {
+            service.register(newUser);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        UserData badUser = new UserData("username", "wrong", "email");
+        assertThrows(UnauthorizedRequestException.class, () -> service.login(badUser));
+    }
+
+    @Test
+    void logoutCorrect() {
+        AuthData auth;
+        try {
+            auth = service.register(newUser);
+            service.logout(auth.authToken());
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        assertNull(authMemory.getAuth(auth.authToken()));
+    }
+
+    @Test
+    void logoutUnauthorized() {
+        assertThrows(UnauthorizedRequestException.class, () -> service.logout(""));
     }
 }
