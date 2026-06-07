ea2e01aefd68cc8c08abda18e622887fd3a430c7
Ben Black <bencoltblack@gmail.com>
2026-06-06 18:11:51 -0600

    Phase 6: Improvement to access username from wsHandler.

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -2,6 +2,9 @@ package server.websocket;
 
 import chess.ChessMove;
 import com.google.gson.Gson;
+import dataaccess.AuthDAO;
+import dataaccess.MySQLAuthDAO;
+import dataaccesserrors.DataAccessException;
 import io.javalin.websocket.*;
 import org.eclipse.jetty.websocket.api.Session;
 import org.jetbrains.annotations.NotNull;
@@ -12,6 +15,7 @@ import java.io.IOException;
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final AuthDAO authMemory = new MySQLAuthDAO();
 
     @Override
     public void handleConnect(@NotNull WsConnectContext ctx) throws Exception {
@@ -24,13 +28,13 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         try {
             UserGameCommand cmd = new Gson().fromJson(ctx.message(), UserGameCommand.class);
             switch (cmd.getCommandType()) {
-                case CONNECT -> connect(cmd.getGameID(), ctx.session);
+                case CONNECT -> connect(cmd.getGameID(), ctx.session, cmd.getAuthToken());
                 case MAKE_MOVE -> makeMove(cmd.getGameID(), ctx.session, cmd.getMove());
                 case LEAVE -> leave(cmd.getGameID(), ctx.session);
                 case RESIGN -> resign(cmd.getGameID(), ctx.session);
             }
         } catch (IOException ex) {
-            ex.printStackTrace();
+            ex.printStackTrace(); // Handle this better
         }
     }
 
@@ -39,10 +43,10 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         System.out.println("Websocket closed");
     }
 
-    private void connect(Integer gameID, Session session) throws IOException {
-        String name = "joey"; // FIGURE OUT HOW TO FIX THIS
+    private void connect(Integer gameID, Session session, String authToken) throws IOException, DataAccessException {
+        String user = getUsername(authToken);
         connections.add(gameID, session);
-        String message = String.format("%s has joined the game", name);
+        String message = String.format("%s has joined the game", user);
         connections.broadcast(gameID, session, message); // Maybe come back and do this with the websocket messages classes?
     }
 
@@ -56,5 +60,9 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     private void resign(Integer gameID, Session session) {
 
+    }
+
+    private String getUsername(String authToken) throws DataAccessException {
+        return authMemory.getAuth(authToken).username();
     }
 }
